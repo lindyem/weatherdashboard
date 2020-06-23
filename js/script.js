4f406a5dd7a8bd355c6b522702555f0c1661f806
lindyem <lindy.merwin@gmail.com>
2020-06-23 16:19:54 -0700

    UV Index

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 //Event Listeners
 $("#searchBtn").click(function () {
   var search = $("#searchInput").val();
-
   var key = "1a53eae6d73210b8584d1690b01b9a73";
+
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=imperial`;
-  // add X
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    // var uvIndex = getUvData(lat, long) //make this function get data from api
-    var uvIndex = 1;
     var icon =
       "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
 
@@ -20,10 +17,31 @@ $("#searchBtn").click(function () {
     $("#temp").text(response.main.temp); //this is how to do it without making vars for each data
     $("#humidity").text(response.main.humidity);
     $("#wind").text(response.wind.speed);
-    $("#uvIndex").text(uvIndex);
-
     $("#weatherInfo").css("display", "block");
+
+    //Get uv index
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${key}&lat=${lat}&lon=${lon}`;
+
+    $.ajax({
+      url: uvURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      $("#uvIndex").text(response.value);
+    });
+
+    //Render forecast
+    $.ajax({
+      url: forecastURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+
   });
 });
+
 //forecast function
-//uvINDEX
+//uvINDEX api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={your api key}
